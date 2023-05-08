a7cfa95dc9b9d21d1910e7515392cffeffe2debd
alanalvarezc2c <129855624+alanalvarezc2c@users.noreply.github.com>
2023-05-08 10:24:18 +0200

    Create metrics.php
    Base file

---
+++ metrics.php
@@ -0,0 +1,30 @@
+<?php
+header('Content-Type: text/plain; charset=utf-8');
+
+$servername = "localhost";
+$username = "aal";
+$password = "063c2582b4cb827726d9018259156dd239a0deb06fce19cf757dd7ba0a898bcb";
+$dbname = "alandb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT COUNT(name) as total_rows FROM people";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $total_rows = $row["total_rows"];
+} else {
+    $total_rows = 0;
+}
+
+$conn->close();
+
+echo "# HELP total_rows_metric Total rows number of 'names'\n";
+echo "# TYPE total_rows_metric gauge\n";
+echo 'total_rows_metric{table="people", column="name"} ' . $total_rows . "\n";
+?>
